208a6a2617a9e3c7c551c40bba19387a4ceb7b92
aaron <aaronherion111@gmail.com>
2017-12-04 11:14:31 -0500

    not done

--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,73 @@
-package io.zipcoder;
-
-public class Problem6Test {
-}
+//package io.zipcoder;
+//
+//import org.junit.Test;
+//
+//public class Problem6Test {
+//
+//
+//    @Test
+//    public void timeGivenTestOne(String timeString){
+//
+//        Problem6 problem = new Problem6();
+//
+//        timeString = "1:30pm";
+//
+//        String expected  = "Thirteen Hundred and Thirty Hours";
+//
+//        String actual = problem.timeGiven(timeString);
+//
+//    }
+//
+//    @Test
+//    public void timeGivenTestTwo(String timeString){
+//
+//        Problem6 problem = new Problem6();
+//
+//        timeString = "1:30am";
+//
+//        String expected  = "Zero One Hundred and Thirty Hours";
+//
+//        String actual = problem.timeGiven(timeString);
+//
+//    }
+//
+//    @Test
+//    public void timeGivenTestThree(String timeString){
+//
+//        Problem6 problem = new Problem6();
+//
+//        timeString = "2:22pm";
+//
+//        String expected  = "Fourteen Hundred and Twenty Two Hours";
+//
+//        String actual = problem.timeGiven(timeString);
+//
+//    }
+//
+//    @Test
+//    public void timeGivenTestThree(String timeString){
+//
+//        Problem6 problem = new Problem6();
+//
+//        timeString = "2:11am";
+//
+//        String expected  = "Zero Two Hundred and Eleven Hours";
+//
+//        String actual = problem.timeGiven(timeString);
+//
+//    }
+//
+//    @Test
+//    public void timeGivenTestThree(String timeString){
+//
+//        Problem6 problem = new Problem6();
+//
+//        timeString = "10:02am";
+//
+//        String expected  = "Ten Hundred and Zero Two Hours";
+//
+//        String actual = problem.timeGiven(timeString);
+//
+//    }
+//
+//}
