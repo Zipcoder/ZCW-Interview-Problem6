5f3ab6f1670e2c168d0d87ed51e93f8b0272786e
pavel <Pavelparfenov112@gmail.com>
2017-12-04 10:28:01 -0500

    "RegEx isn't quite as good as streams but still saves the world."

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,132 @@
 package io.zipcoder;
 
 public class Problem6 {
+
+    public String normalToMilitaryTime(String input) {
+        String militarizedTime = militarizeTime(input);
+        return convertToString(militarizedTime);
+    }
+
+    String convertToString(String militarizedTime) {
+        String output = "";
+        String hours = twoDigitToString(militarizedTime.substring(0,2));
+        String minutes = twoDigitToString(militarizedTime.substring(3));
+        output = String.format("%s Hundred and %s Hours",hours,minutes);
+        return output;
+    }
+
+    String twoDigitToString(String twoDigit) {
+        String output = "";
+        String first = twoDigit.substring(0,1);
+        String second = twoDigit.substring(1);
+
+        if("1".equals(first)){
+            switch(second){
+                case "0":
+                    output += "Ten";
+                    break;
+                case "1":
+                    output += "Eleven";
+                    break;
+                case "2":
+                    output += "Twelve";
+                    break;
+                case "3":
+                    output += "Thirteen";
+                    break;
+                case "4":
+                    output += "Fourteen";
+                    break;
+                case "5":
+                    output += "Fifteen";
+                    break;
+                case "6":
+                    output += "Sixteen";
+                    break;
+                case "7":
+                    output += "Seventeen";
+                    break;
+                case "8":
+                    output += "Eighteen";
+                    break;
+                case "9":
+                    output += "Nineteen";
+                    break;
+            }
+            return output;
+        }
+
+        switch(first){
+            case "0":
+                output += "Zero";
+                break;
+            case "2":
+                output += "Twenty";
+                break;
+            case "3":
+                output += "Thirty";
+                break;
+            case "4":
+                output += "Forty";
+                break;
+            case "5":
+                output += "Fifty";
+                break;
+        }
+
+        switch(second){
+            case "0":
+                output += "";
+                break;
+            case "1":
+                output += " One";
+                break;
+            case "2":
+                output += " Two";
+                break;
+            case "3":
+                output += " Three";
+                break;
+            case "4":
+                output += " Four";
+                break;
+            case "5":
+                output += " Five";
+                break;
+            case "6":
+                output += " Six";
+                break;
+            case "7":
+                output += " Seven";
+                break;
+            case "8":
+                output += " Eight";
+                break;
+            case "9":
+                output += " Nine";
+                break;
+        }
+
+        return output;
+    }
+
+    String militarizeTime(String normalTime){
+        String output = "";
+        int colonIndex = normalTime.indexOf(':');
+        if(normalTime.matches(".*pm")){
+            int hours = Integer.parseInt(normalTime.substring(0, colonIndex))+12;
+            if(hours == 24){
+                output = "00"+normalTime.substring(colonIndex, colonIndex+3);
+            } else {
+                output = hours + normalTime.substring(colonIndex, colonIndex+3);
+            }
+        } else {
+            output = normalTime.substring(0, colonIndex+3);
+        }
+        if(output.indexOf(':') == 1){
+            output = "0"+output;
+        }
+        return output;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,117 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem6Test {
+
+    @Test
+    public void normalToMilitaryTimeTest1(){
+        String input = "1:30pm";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Thirteen Hundred and Thirty Hours";
+        String actual = problem6.normalToMilitaryTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void normalToMilitaryTimeTest2(){
+        String input = "1:30am";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Zero One Hundred and Thirty Hours";
+        String actual = problem6.normalToMilitaryTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void normalToMilitaryTimeTest3(){
+        String input = "2:22pm";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Fourteen Hundred and Twenty Two Hours";
+        String actual = problem6.normalToMilitaryTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void normalToMilitaryTimeTest4(){
+        String input = "2:11am";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Zero Two Hundred and Eleven Hours";
+        String actual = problem6.normalToMilitaryTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void normalToMilitaryTimeTest5(){
+        String input = "10:02am";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Ten Hundred and Zero Two Hours";
+        String actual = problem6.normalToMilitaryTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void militarizeTimeTest(){
+        String input = "10:02pm";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "22:02";
+        String actual = problem6.militarizeTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void twoDigitToStringTwentyTwoTest(){
+        String input = "22";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Twenty Two";
+        String actual = problem6.twoDigitToString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void twoDigitToStringThirteenTest(){
+        String input = "13";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Thirteen";
+        String actual = problem6.twoDigitToString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void twoDigitToStringZeroFiveTest(){
+        String input = "05";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Zero Five";
+        String actual = problem6.twoDigitToString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertToStringTest(){
+        String input = "22:02";
+        Problem6 problem6 = new Problem6();
+
+        String expected = "Twenty Two Hundred and Zero Two Hours";
+        String actual = problem6.convertToString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
