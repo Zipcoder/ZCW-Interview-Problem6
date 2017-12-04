8beb167019fee1aa624901ad8d30e7d094c341ba
lina <you@example.com>
2017-12-04 11:30:34 -0500

    not there yet

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Problem6 {
+    //Step 1: convert given 12 hour numerical format into 24 hours format
+    //use SimpleDateFormat to format input to 24 hours
+    public String convertTo24Hours(String input){
+        SimpleDateFormat displayFormat = new SimpleDateFormat("HH:mm");
+        SimpleDateFormat parseFormat = new SimpleDateFormat("hh:mm a");
+        Date date= null;
+        try {
+            date = parseFormat.parse(input);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return displayFormat.format(date);
+    }
+    //Step 2: convert the 24 hour format into military time phrase in string format
+    //after the first two digit, add "Hundred and" to the end
+    //add Hours at the end of each string
+    public String convert(String input){
+
+        String[] units= { "Zero", "One", "Two", "Three", "Four",
+                "Five", "Six", "Seven", "Eight", "Nine","Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty",
+                "Sixty", "Seventy", "Eighty", "Ninety"};
+        String formattedInput = convertTo24Hours(input);
+        String[] splitFormattedInput = formattedInput.split(":");
+        for(int i=0;i<splitFormattedInput.length;i++){
+            int num= Integer.parseInt(splitFormattedInput[i]);
+            if(num<20){
+                splitFormattedInput[i]=units[i];
+            }
+            else{
+                splitFormattedInput[i]=tens[i];
+            }
+        }
+        return splitFormattedInput[0]+" Hundred and "+splitFormattedInput[1]+" Hours";
+    }
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem6Test {
+    Problem6 problem6 = new Problem6();
+    @Test
+    public void testConvertTo24Hours(){
+        String expected= "14:30";
+        String actual = problem6.convertTo24Hours("2:30 pm");
+        Assert.assertEquals(expected,actual);
+        System.out.println(actual);
+    }
+    //output should Fourteen Hundred and Thirty Hours
+    @Test
+    public void testConvert(){
+        String expected="Fourteen Hundred and Thirty Hours";
+        String actual = problem6.convert("2:30 pm");
+        Assert.assertEquals(expected,actual);
+    }
 }
