86f47beb9b0491019a7385a42b52b0677620c497
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-12-04 09:58:04 -0500

    Katherine Anderson military time phrase convert app

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,99 @@
 package io.zipcoder;
 
+import java.util.StringJoiner;
+
 public class Problem6 {
+
+    public String convertTimeToMilitaryTimePhrase(String time) {
+        Integer[] hoursAndMinutes = splitIntoHoursAndMinutes(time);
+        return createMilitaryPhrase(hoursAndMinutes);
+    }
+
+    public Integer[] splitIntoHoursAndMinutes(String time) {
+        String[] timeParts = time.split(":");
+        String amOrPm = timeParts[1].substring(2);
+        int hours = Integer.parseInt(timeParts[0]);
+        if(hours != 12) {
+            if ("pm".equals(amOrPm)) {
+                hours += 12;
+            }
+        }
+        else {
+            if("am".equals(amOrPm)) {
+                hours -= 12;
+            }
+        }
+        int minutes = Integer.parseInt(timeParts[1].substring(0, 2));
+
+        Integer[] hoursAndMinutes = new Integer[2];
+        hoursAndMinutes[0] = hours;
+        hoursAndMinutes[1] = minutes;
+
+        return hoursAndMinutes;
+    }
+
+    public String createMilitaryPhrase(Integer[] hoursAndMinutes) {
+        int hours = hoursAndMinutes[0];
+        int minutes = hoursAndMinutes[1];
+
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if(hours < 10) {
+            stringJoiner.add(numAsString[0]);
+        }
+
+        if(hours <= 20) {
+            stringJoiner.add(numAsString[hours]);
+        }
+        else {
+            stringJoiner.add(numAsString[20] + numAsString[hours - 20]);
+        }
+
+        stringJoiner.add("Hundred");
+        stringJoiner.add("and");
+
+        if(minutes <= 20) {
+            if(minutes < 10) {
+                stringJoiner.add(numAsString[0]);
+            }
+            stringJoiner.add(numAsString[minutes]);
+        }
+        else {
+            int tens = minutes/10;
+            int ones = minutes - tens*10;
+            stringJoiner.add(numAsString[18 + tens]);
+            if(ones > 0) {
+                stringJoiner.add(numAsString[ones]);
+            }
+        }
+
+        stringJoiner.add("Hours");
+        return stringJoiner.toString();
+    }
+
+    public String[] numAsString = {
+            "Zero",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Eleven",
+            "Twelve",
+            "Thirteen",
+            "Fourteen",
+            "Fifteen",
+            "Sixteen",
+            "Seventeen",
+            "Eighteen",
+            "Nineteen",
+            "Twenty",
+            "Thirty",
+            "Forty",
+            "Fifty"
+    };
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,85 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem6Test {
+
+    Problem6 problem6;
+
+    @Before
+    public void initialize() {
+        problem6 = new Problem6();
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseOneThirtyPMTest() {
+        String input = "1:30pm";
+
+        String expected = "Thirteen Hundred and Thirty Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseOneThirtyAMTest() {
+        String input = "1:30am";
+
+        String expected = "Zero One Hundred and Thirty Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseTwoTwentyTwoPMTest() {
+        String input = "2:22pm";
+
+        String expected = "Fourteen Hundred and Twenty Two Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseTwoElevenAMTest() {
+        String input = "2:11am";
+
+        String expected = "Zero Two Hundred and Eleven Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseTenOTwoAMTest() {
+        String input = "10:02am";
+
+        String expected = "Ten Hundred and Zero Two Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseTwelveFiftyFivePMTest() {
+        String input = "12:55pm";
+
+        String expected = "Twelve Hundred and Fifty Five Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertTimeToMilitaryTimePhraseTwelveSeventeenAMTest() {
+        String input = "12:17am";
+
+        String expected = "Zero Zero Hundred and Seventeen Hours";
+        String actual = problem6.convertTimeToMilitaryTimePhrase(input);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
