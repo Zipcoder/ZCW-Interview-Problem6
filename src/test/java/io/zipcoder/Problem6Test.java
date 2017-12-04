7d824980bbb0b7dec3ae967a61be56d6b5732cd8
rob <rob@zipcoders-MacBook-Pro.local>
2017-12-04 10:30:07 -0500

    done

--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,38 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem6Test {
+
+    @Test
+    public void getPeriodOfDayTest(){
+        String input = "1:30am";
+
+        String expected = "am";
+        String actual = Problem6.getPeriodOfDay(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTimeTest(){
+        String input = "1:30pm";
+
+        String expected = "1:30";
+        String actual = Problem6.getTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void determineTimeTest(){
+        String input = "1:30pm";
+
+        String expected = "Ten Hundred and Zero Two Hours";
+        String actual = Problem6.determineTime(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
