2bb3ca4e935e41f87920fe001264b3425f797763
nehctuk <nehctuk@gmail.com>
2017-12-04 10:40:03 -0500

    Problem 6

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,90 @@
 package io.zipcoder;
 
 public class Problem6 {
+    private String[] upToNineteen = { "", "One", "Two", "Three", "Four",
+            "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+            "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+            "Eighteen", "Nineteen" };
+
+    private String[] aboveTwenty = {"", "","Twenty","Thirty","Forty","Fifty","Sixty"};
+
+    private String convertTime(int time) {
+        if (time < 20) {
+            return upToNineteen[time];
+        }
+
+        else if (time <= 60) {
+            return aboveTwenty[time / 10] + ((time % 10 != 0) ? " " : "") + upToNineteen[time % 10];
+        }
+        return null;
+    }
+
+    private Integer numericalMilitaryHours(String input) {
+        return Integer.parseInt(input.split(":")[0]);
+    }
+
+    private Integer numericalMilitaryMinutes(String input) {
+        String backEnd = input.split(":")[1];
+        if (backEnd.substring(0,1).equalsIgnoreCase("0")) {
+            Integer aInt = Integer.parseInt(backEnd.substring(1,2));
+            return aInt;
+        }
+        else {
+            return Integer.parseInt(backEnd.substring(0,2));
+        }
+    }
+
+    private String amOrPm(String input) {
+        return input.split(":")[1].substring(2,4);
+    }
+
+    public String numericalMilitaryTime(String input) {
+        Integer hours = numericalMilitaryHours(input);
+        Integer minutes = numericalMilitaryMinutes(input);
+        String nightOrDay = amOrPm(input);
+        Integer militaryHours;
+        if (nightOrDay.equalsIgnoreCase("pm")) {
+            militaryHours = hours + 12;
+        }
+        else {
+            militaryHours = hours;
+        }
+
+        String stringMinutes = "";
+        if (minutes<10) {
+            stringMinutes += "0"+minutes;
+        }
+        else {
+            stringMinutes += minutes;
+        }
+
+        String militaryNumericalTime = militaryHours + ":" + stringMinutes;
+        return militaryNumericalTime;
+    }
+
+    public String wordFromOfMilitaryTime(String input) {
+        String militaryTimeString = numericalMilitaryTime(input);
+        Integer hours = numericalMilitaryHours(militaryTimeString);
+        Integer minutes = numericalMilitaryMinutes(militaryTimeString);
+        String militaryHoursWordForm;
+        String militaryMinutesWordForm;
+        if (hours<10){
+            militaryHoursWordForm = "Zero "+ convertTime(hours);
+        }
+        else {
+            militaryHoursWordForm = convertTime(hours);
+        }
+        if (minutes<10) {
+            militaryMinutesWordForm = "Zero "+ convertTime(minutes);
+        }
+        else {
+            militaryMinutesWordForm = convertTime(minutes);
+        }
+
+        String wordOfMilitaryTime = militaryHoursWordForm+" Hundred and "+ militaryMinutesWordForm+" Hours";
+        return wordOfMilitaryTime;
+    }
+
+
+
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,66 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem6Test {
+    Problem6 problem6;
+    String input1;
+    String input2;
+    String input3;
+    String input4;
+    String input5;
+
+    @Before
+    public void setup() {
+        problem6 = new Problem6();
+        input1 = "1:30pm";
+        input2 = "1:30am";
+        input3 = "2:22pm";
+        input4 = "2:11am";
+        input5 = "10:02am";
+    }
+
+    @Test
+    public void numericalMilitaryTimeTest() {
+        String expected = "13:30";
+        String actual = problem6.numericalMilitaryTime(input1);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void wordFromOfMilitaryTimeTest_130pm() {
+        String expected = "Thirteen Hundred and Thirty Hours";
+        String actual = problem6.wordFromOfMilitaryTime(input1);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void wordFromOfMilitaryTimeTest_130am() {
+        String expected = "Zero One Hundred and Thirty Hours";
+        String actual = problem6.wordFromOfMilitaryTime(input2);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void wordFromOfMilitaryTimeTest_222pm() {
+        String expected = "Fourteen Hundred and Twenty Two Hours";
+        String actual = problem6.wordFromOfMilitaryTime(input3);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void wordFromOfMilitaryTimeTest_211pm() {
+        String expected = "Zero Two Hundred and Eleven Hours";
+        String actual = problem6.wordFromOfMilitaryTime(input4);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void wordFromOfMilitaryTimeTest_1002am() {
+        String expected = "Ten Hundred and Zero Two Hours";
+        String actual = problem6.wordFromOfMilitaryTime(input5);
+        Assert.assertEquals(expected,actual);
+    }
 }
