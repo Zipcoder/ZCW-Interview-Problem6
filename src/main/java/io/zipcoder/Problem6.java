81ece001513934a0dad93618ec0095cd56d8bf6f
pavel <Pavelparfenov112@gmail.com>
2017-12-04 10:42:31 -0500

    "Uncle Bob says kill switch statements with fire. Replaced with Hashmaps"

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,18 +1,17 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+
 public class Problem6 {
 
     public String normalToMilitaryTime(String input) {
-        String militarizedTime = militarizeTime(input);
-        return convertToString(militarizedTime);
+        return convertToString(militarizeTime(input));
     }
 
     String convertToString(String militarizedTime) {
-        String output = "";
         String hours = twoDigitToString(militarizedTime.substring(0,2));
         String minutes = twoDigitToString(militarizedTime.substring(3));
-        output = String.format("%s Hundred and %s Hours",hours,minutes);
-        return output;
+        return String.format("%s Hundred and %s Hours",hours,minutes);
     }
 
     String twoDigitToString(String twoDigit) {
@@ -20,92 +19,48 @@ public class Problem6 {
         String first = twoDigit.substring(0,1);
         String second = twoDigit.substring(1);
 
+        HashMap<String, String> teens = new HashMap<String, String>(){{
+            put("0","Ten");
+            put("1", "Eleven");
+            put("2", "Twelve");
+            put("3", "Thirteen");
+            put("4", "Fourteen");
+            put("5", "Fifteen");
+            put("6", "Sixteen");
+            put("7", "Seventeen");
+            put("8", "Eighteen");
+            put("9", "Nineteen");
+        }};
+
         if("1".equals(first)){
-            switch(second){
-                case "0":
-                    output += "Ten";
-                    break;
-                case "1":
-                    output += "Eleven";
-                    break;
-                case "2":
-                    output += "Twelve";
-                    break;
-                case "3":
-                    output += "Thirteen";
-                    break;
-                case "4":
-                    output += "Fourteen";
-                    break;
-                case "5":
-                    output += "Fifteen";
-                    break;
-                case "6":
-                    output += "Sixteen";
-                    break;
-                case "7":
-                    output += "Seventeen";
-                    break;
-                case "8":
-                    output += "Eighteen";
-                    break;
-                case "9":
-                    output += "Nineteen";
-                    break;
-            }
+            output += teens.get(second);
             return output;
         }
 
-        switch(first){
-            case "0":
-                output += "Zero";
-                break;
-            case "2":
-                output += "Twenty";
-                break;
-            case "3":
-                output += "Thirty";
-                break;
-            case "4":
-                output += "Forty";
-                break;
-            case "5":
-                output += "Fifty";
-                break;
-        }
+        HashMap<String, String> tens = new HashMap<String, String>(){{
+            put("0","Zero");
+            put("2", "Twenty");
+            put("3", "Thirty");
+            put("4", "Forty");
+            put("5", "Fifty");
+        }};
 
-        switch(second){
-            case "0":
-                output += "";
-                break;
-            case "1":
-                output += " One";
-                break;
-            case "2":
-                output += " Two";
-                break;
-            case "3":
-                output += " Three";
-                break;
-            case "4":
-                output += " Four";
-                break;
-            case "5":
-                output += " Five";
-                break;
-            case "6":
-                output += " Six";
-                break;
-            case "7":
-                output += " Seven";
-                break;
-            case "8":
-                output += " Eight";
-                break;
-            case "9":
-                output += " Nine";
-                break;
-        }
+        output += tens.get(first);
+
+        HashMap<String, String> ones = new HashMap<String, String>(){{
+            put("0","");
+            put("1", " One");
+            put("2", " Two");
+            put("3", " Three");
+            put("4", " Four");
+            put("5", " Five");
+            put("6", " Six");
+            put("7", " Seven");
+            put("8", " Eight");
+            put("9", " Nine");
+        }};
+
+        output += ones.get(second);
 
         return output;
     }
