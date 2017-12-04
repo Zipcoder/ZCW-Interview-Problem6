814013cb0e3ca34ea3ad6beda152c44f84a657dc
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-12-04 11:10:51 -0500

    not finished

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
 public class Problem6 {
+
+    public String convertGivenTimeToMilitaryTime(String givenTime){
+        StringBuilder militaryTime = new StringBuilder().append(givenTime);
+
+        int hour = Integer.parseInt(givenTime.substring(0,2));
+        String amPM = givenTime.substring(8,10);
+
+        if (amPM.equals("am") && hour == 12)
+            militaryTime.replace(0, 2, "00");
+        else if (amPM.equals("pm") && hour < 12) {
+            militaryTime.replace(0, 2, String.valueOf(hour + 12));
+        }
+        militaryTime.replace(8, 10, "");
+        return militaryTime.toString();
+    }
+
+//    public String convertMilitaryTimeToPhrase(){
+//    }
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,61 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem6Test {
+
+    Problem6 problem6;
+    String givenTime;
+
+    @Before
+    public void setup(){
+        problem6 = new Problem6();
+        givenTime = "";
+
+    }
+    @Test
+    public void convertGivenTimeToMilitaryTime() throws Exception{
+        String expected = "1330";
+        String actual = problem6.convertGivenTimeToMilitaryTime("1:30pm");
+        Assert.assertEquals(expected,actual);
+
+    }
+
+//    @Test
+//    public void convertToMilitaryTimeTest1() throws Exception {
+//        String expected = "Thirteen Hundred and Thirty Hours";
+//        String actual = problem6.convertMilitaryTimeToPhrase("1330");
+//        Assert.assertEquals(expected, actual);
+//
+//    }
+//
+//    @Test
+//    public void convertToMilitaryTimeTest2() throws Exception {
+//        String expected = "Zero One Hundred and Thirty Hours";
+//        String actual = problem6.convertMilitaryTimeToPhrase("0130");
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void convertToMilitaryTimeTest3() throws Exception {
+//        String expected = "Fourteen Hundred and Twenty Two Hours";
+//        String actual = problem6.convertMilitaryTimeToPhrase("1422");
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void convertToMilitaryTimeTest4() throws Exception {
+//        String expected = "Zero Two Hundred and Eleven Hour";
+//        String actual = problem6.convertMilitaryTimeToPhrase("0211");
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void convertToMilitaryTimeTest5() throws Exception {
+//        String expected = "Ten Hundred Zero Two Hours";
+//        String actual = problem6.convertMilitaryTimeToPhrase("1002");
+//        Assert.assertEquals(expected, actual);
+//    }
 }
