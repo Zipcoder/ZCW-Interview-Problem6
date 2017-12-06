37be1a39a490b60bc11be58333d758cd4bd5ab6a
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-12-05 19:29:26 -0500

    finished

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -2,36 +2,47 @@ package io.zipcoder;
 
 public class Problem6 {
 
-    public String convertGivenTimeToMilitaryTime(String givenTime){
+    public String convertGivenTimeToMilitaryTime(String givenTime) {
         String[] splitInput = givenTime.split(":");
         int hour = Integer.parseInt(splitInput[0]);
-        int minute = Integer.parseInt(splitInput[1].substring(0,2));
+        int minute = Integer.parseInt(splitInput[1].substring(0, 2));
         String meridian = splitInput[1].substring(2);
 
-        if(meridian.equals("pm")) {
+        if (meridian.equals("pm")) {
             hour += 12;
         }
 
         String hourName = convertNumberToWord(hour);
         String minuteName = convertNumberToWord(minute);
 
-        String output = hourName + " Hundred " + "and " + minuteName + " Hours";
-
-        return output;
+        if(minute == 0){
+            return hourName + " Hundred Hours";
+        }
+        if(minute < 10){
+            return hourName + " Hundred " + minuteName + " Hours";
+        } else {
+            return hourName + " Hundred and " + minuteName + " Hours";
+        }
     }
 
-    public static String convertNumberToWord(int n){
-        //take a military time and convert it to a phrase
-        String[] upToNotIncludingTwenty = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+    public static String convertNumberToWord(int number) {
+        String[] lessThanTwenty = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
                 "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
         String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty"};
 
-        if(n < 20){
-            return upToNotIncludingTwenty[n];
-        }
-        else  {
-            return tens[5 / n];
+        if (number < 20) {
+            if (number < 10 && number > 0) {
+                return lessThanTwenty[0] + " " + lessThanTwenty[number];
+            } else {
+                return lessThanTwenty[number];
+            }
+        } else if (number % 10 == 0) {
+            return tens[number / 10];
+        } else {
+            String output = tens[(int) Math.floor(number / 10)];
+            return output += " " + lessThanTwenty[(int) (number - (Math.floor(number / 10) * 10))];
         }
     }
 }
+//violates SRP will make better I promise.
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -5,57 +5,52 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class Problem6Test {
-
     Problem6 problem6;
-    String givenTime;
 
     @Before
     public void setup(){
         problem6 = new Problem6();
-        givenTime = "";
-
-    }
-    @Test
-    public void convertGivenTimeToMilitaryTimeTest() throws Exception{
-        String input = "1:30pm";
-        String expected = "1330";
-        String actual = problem6.convertGivenTimeToMilitaryTime(input);
-        Assert.assertEquals(expected,actual);
 
     }
 
     @Test
-    public void convertNumberToWordTest1() throws Exception {
+    public void convertNumberToWord() throws Exception {
+        String expected = "Zero Five";
+        String actual = Problem6.convertNumberToWord(5);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertGivenTimeToMilitaryTimeTest1() throws Exception {
         String expected = "Thirteen Hundred and Thirty Hours";
         String actual = problem6.convertGivenTimeToMilitaryTime("1:30pm");
         Assert.assertEquals(expected, actual);
-
     }
 
     @Test
-    public void convertNumberToWordTest2() throws Exception {
+    public void convertGivenTimeToMilitaryTimeTest2() throws Exception {
         String expected = "Zero One Hundred and Thirty Hours";
         String actual = problem6.convertGivenTimeToMilitaryTime("1:30am");
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void convertNumberToWordTest3() throws Exception {
+    public void convertGivenTimeToMilitaryTimeTest3() throws Exception {
         String expected = "Fourteen Hundred and Twenty Two Hours";
         String actual = problem6.convertGivenTimeToMilitaryTime("2:22pm");
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void convertNumberToWordTest4() throws Exception {
-        String expected = "Zero Two Hundred and Eleven Hour";
+    public void convertGivenTimeToMilitaryTimeTest4() throws Exception {
+        String expected = "Zero Two Hundred and Eleven Hours";
         String actual = problem6.convertGivenTimeToMilitaryTime("2:11am");
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void convertNumberToWordTest5() throws Exception {
-        String expected = "Ten Hundred Zero and Two Hours";
+    public void convertGivenTimeToMilitaryTimeTest5() throws Exception {
+        String expected = "Ten Hundred Zero Two Hours";
         String actual = problem6.convertGivenTimeToMilitaryTime("10:02am");
         Assert.assertEquals(expected, actual);
     }
