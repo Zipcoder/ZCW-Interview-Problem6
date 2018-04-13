325127bda6e1f9d9c3976ced7a3e79bc329703cc
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-04-13 09:56:29 -0400

    TimeConverter unit tests written

---
+++ src/main/java/io/zipcoder/TimeConverter.java
@@ -0,0 +1,13 @@
+package io.zipcoder;
+
+public class TimeConverter {
+
+    public static String[] splitTime(String time){
+        return null;
+    }
+
+    public static String[] convertTime(String [] input){
+        return null;
+    }
+
+}
