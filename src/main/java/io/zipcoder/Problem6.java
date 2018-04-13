94c63453d29fa002fb19a78bed724cbbb4bfac61
Garrett Arant <garrettarant@MacBook-Pro-6.local>
2018-04-13 14:20:56 -0400

    Completed

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,43 @@
 package io.zipcoder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem6 {
+
+    public String militaryTime(String input) {
+
+        String standardTimePattern = "(1[012]|[1-9]):([0-5][0-9])(am|pm)";
+        Pattern pattern = Pattern.compile(standardTimePattern);
+        Matcher matcher = pattern.matcher(input);
+
+
+        String[] minutesAndHours = {"Zero Zero", "Zero One", "Zero Two","Zero Three","Zero Four","Zero Five","Zero Six","Zero Seven", "Zero Eight", "Zero Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Twenty One", "Twenty Two", "Twenty Three", "Twenty Four", "Twenty Five", "Twenty Six", "Twenty Seven", "Twenty Eight", "Twenty Nine",
+                "Thirty", "Thirty One", "Thirty Two", "Thirty Three", "Thirty Four", "Thirty Five", "Thirty Six", "Thirty Seven", "Thirty Eight", "Thirty Nine", "Forty", "Forty One", "Forty Two", "Fourty Three", "Forty Four",
+                "Forty Five", "Forty Six", "Forty Seven", "Forty Eight", "Forty Nine", "Fifty", "Fifty One", "Fifty Two", "Fifty Three", "Fifty Four", "Fifty Five", "Fifty Six", "Fifty Seven", "Fifty Eight", "Fifty Nine"};
+
+
+        Integer[] intArray = new Integer[2];
+        if(matcher.find()){
+            if(matcher.group(3).equals("pm")){
+                intArray[0] = Integer.valueOf(matcher.group(1)) + 12;
+            } else {
+                intArray[0] = Integer.valueOf(matcher.group(1));
+            }
+            intArray[1] = Integer.valueOf(matcher.group(2));
+        }
+
+        String militaryTime ="";
+        if(intArray[1] < 10){
+            militaryTime = minutesAndHours[intArray[0]] + " Hundred " + minutesAndHours[intArray[1]]+ " Hours";
+
+        }
+        else {
+            militaryTime= minutesAndHours[intArray[0]] + " Hundred and " + minutesAndHours[intArray[1]] + " Hours";
+        }
+        return militaryTime;
+
+    }
+
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,66 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class Problem6Test {
+
+    @Test
+    public void testMiltaryTime1()  {
+        //Given
+        String input = "1:30pm";
+        Problem6 problem6 = new Problem6();
+
+        //When
+        String expected = "Thirteen Hundred and Thirty Hours";
+        String actual = problem6.militaryTime(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMiltaryTime2()  {
+        //Given
+        String input = "1:30am";
+        Problem6 problem6 = new Problem6();
+
+        //When
+        String expected = "Zero One Hundred and Thirty Hours";
+        String actual = problem6.militaryTime(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMiltaryTime3()  {
+        //Given
+        String input = "2:11am";
+        Problem6 problem6 = new Problem6();
+
+        //When
+        String expected = "Zero Two Hundred and Eleven Hours";
+        String actual = problem6.militaryTime(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMiltaryTime4()  {
+        //Given
+        String input = "10:02am";
+        Problem6 problem6 = new Problem6();
+
+        //When
+        String expected = "Ten Hundred Zero Two Hours";
+        String actual = problem6.militaryTime(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
