90188648a300e86afcf5fdc5a945a2498766fcbe
Patrick Glavin <patjglavin@gmail.com>
2018-04-13 10:17:25 -0400

    prep for convertClass

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,58 @@
 package io.zipcoder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem6 {
+    public String militaryTime(String inputTime){
+        String result = "";
+        String regexFormula = "([0-9]*):([0-9]{2})([ap])";
+        Pattern pattern = Pattern.compile(regexFormula);
+        Matcher matcher = pattern.matcher(inputTime);
+        int hour = 0;
+        int minute = 0;
+        String meridiem = "";
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+            hour = Integer.parseInt(matcher.group(1));
+            System.out.println(matcher.group(2));
+            minute = Integer.parseInt(matcher.group(2));
+            System.out.println(matcher.group(3));
+            meridiem = matcher.group(3);
+        }
+        if (meridiem.equals("p")){
+            hour += 12;
+        }
+        String stringHour = parseHour(hour);
+        result += stringHour;
+        return result;
+    }
+
+    private String parseHour(int number){
+        String result = "";
+        if (number < 10){
+            result += "Zero ";
+        }
+        result += parseNumber(number);
+        return result;
+    }
+
+    private String parseNumber(int number){
+        String numberResult = "";
+        switch (number){
+            case 1:
+                numberResult += "One";
+                break;
+            case 2:
+                numberResult += "Two";
+                break;
+            case 3:
+                numberResult += "Three";
+                break;
+            case 4:
+                numberResult += "Four";
+                break;
+        }
+        return numberResult;
+    }
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem6Test {
+    Problem6 problem6;
+
+    @Before
+    public void setup(){
+        problem6 = new Problem6();
+    }
+
+    @Test
+    public void onethirtyTest(){
+        String actual = problem6.militaryTime("1:30am");
+        String expected = "Zero One Hundred and Thirty Hours";
+        Assert.assertEquals(expected, actual);
+    }
 }
