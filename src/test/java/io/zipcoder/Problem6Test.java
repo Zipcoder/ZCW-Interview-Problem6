16780664a2d2303755edcce8ca140a30a36bdb8c
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-15 19:37:02 -0400

    gonna add enums later

--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,49 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem6Test {
+
+
+    @Test
+    public void militaryTimeTest(){
+        String time = "1:30pm";
+        String militaryTime = "Thirteen Hundred and Thirty Hours";
+        Problem6 testProblem6 = new Problem6();
+        String actual = testProblem6.militaryConverter(time);
+        Assert.assertEquals(militaryTime, actual);
+    }
+    @Test
+    public void militaryTimeTest1(){
+        String time = "1:30am";
+        String militaryTime = "Zero One Hundred and Thirty Hours";
+        Problem6 testProblem6 = new Problem6();
+        String actual = testProblem6.militaryConverter(time);
+        Assert.assertEquals(militaryTime, actual);
+    }
+    @Test
+    public void militaryTimeTest2(){
+        String time = "2:22pm";
+        String militaryTime = "Fourteen Hundred and Twenty Two Hours";
+        Problem6 testProblem6 = new Problem6();
+        String actual = testProblem6.militaryConverter(time);
+        Assert.assertEquals(militaryTime, actual);
+    }
+    @Test
+    public void militaryTimeTest3(){
+        String time = "2:11am";
+        String militaryTime = "Zero Two Hundred and Eleven Hours";
+        Problem6 testProblem6 = new Problem6();
+        String actual = testProblem6.militaryConverter(time);
+        Assert.assertEquals(militaryTime, actual);
+    }
+    @Test
+    public void militaryTimeTest4(){
+        String time = "10:02am";
+        String militaryTime = "Ten Hundred and Zero Two Hours";
+        Problem6 testProblem6 = new Problem6();
+        String actual = testProblem6.militaryConverter(time);
+        Assert.assertEquals(militaryTime, actual);
+    }
 }
