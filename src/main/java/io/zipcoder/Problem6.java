45f79edeab42c41456eb3d94236d56f4d17382db
ahsonali <ahsonac@gmail.com>
2018-04-16 21:45:41 -0400

    parsed only, more to do

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,113 @@
 package io.zipcoder;
 
 public class Problem6 {
+
+    private String militaryHour;
+    private String militaryMinute;
+
+    private String[] hoursArray = {"Zero-zero", "Zero One Hundred ", "Zero Two Hundred ", "Zero Three Hundred ",
+                                   "Zero Four Hundred ", "Zero Five Hundred ", "Zero Six Hundred ", " Zero Seven Hundred ",
+                                    "Zero Eight Hundred ", "Zero Nine Hundred ", "Ten Hundred ", "Eleven Hundred ",
+                                    "Twelve Hundred ", "Thirteen Hundred ", "Fourteen Hundred ", "Fifteen Hundred ",
+                                    "Sixteen Hundred ", "Seventeen Hundred ", "Eighteen Hundred ", "Nineteen Hundred ",
+                                    "Twenty Hundred ", "Twenty One Hundred ", "Twenty Two Hundred ", "Twenty Three Hundred ",
+                                    "Twenty Four Hundred "};
+    private String[] minutesArray = {"", "and One", "and Two", "and Three", "and Four", "and Five", "and Six", "and Seven",
+                                    "and Eight", "and Nine", "and Ten", "and Eleven", "and Twelve", "and Thirteen",
+                                    "and Fourteen", "and Fifteen", "and Sixteen", "and Seventeen", "and Eighteen",
+                                    "and Nineteen", "and Twenty", "and Twenty One", "and Twenty Two", "and Twenty Three",
+                                    "and Twenty Four", "and Twenty Five", "and Twenty Six", "and Twenty Seven",
+                                    "and Twenty Eight", "and Twenty Nine", "and Thirty", "and Thirty One",
+                                    "and Thirty One", "and Thirty Two", "and Thirty Three", "and Thirty Four",
+                                    "and Thirty Five", "and Thirty Six", "and Thirty Seven", "and Thirty Eight",
+                                    "and Thirty Nine", "and Fourty", "and Fourty One", "and Fourty Two", "and Fourty Three",
+                                    "and Fourty Four", "and Fourty Five", "and Fourty Six", "and Fourty Seven",
+                                    "and Fourty Eight", "and Fourty Nine", "and Fifty", "and Fifty One", "and Fifty Two",
+                                    "and Fifty Three", "and Fifty Four", "and Fifty Five", "and Fifty Six", "and Fifty Seven",
+                                    "and Fifty Eight", "and Fifty Nine", "zero-zero"};
+
+    public String[] splitTime(String timeInput)
+    {
+        String [] splitHourAndMinute = timeInput.split(":");
+        return splitHourAndMinute;
+    }
+
+    public Integer parseForHour(String timeInput)
+    {
+        String [] hour = splitTime(timeInput);
+        Integer intHour = Integer.parseInt(hour[0]);
+
+        if(hour[1].contains("p") || hour[1].contains("P"))
+        {
+            return intHour += 12;
+        }
+        else {
+            return intHour;
+        }
+    }
+
+    public Integer parseForMinute(String timeInput)
+    {
+        String [] minute = splitTime(timeInput);
+        String minutePortion = minute[1];
+
+        //Using Stringbuilder to append minute without the am or pm
+        StringBuilder noAmOrPm = new StringBuilder();
+        noAmOrPm.append(minutePortion.charAt(0));
+        noAmOrPm.append(minutePortion.charAt(1));
+
+        //StringBuilder to String
+        String minuteToString = noAmOrPm.toString();
+
+        //String parsed to Integer
+        Integer minuteOnly = Integer.parseInt(minuteToString);
+
+        return minuteOnly;
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        this.hour = hour;
+//        this.amOrPm = amOrPm;
+
+
+//    public static String timeToString(String digitalTime){
+//
+//
+//
+//        return null;
+//    }
+
+//private static int hour;
+//private String minute
+//private final String timeColon = ":"
+//private static String amOrPm;
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,185 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem6Test {
+
+    @Test
+    public void testSplitTime()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "1:30pm";
+        String hour = "1";
+        String minute = "30";
+        String [] expected = {"1", "30pm"};
+
+        //When
+        String [] actual = problem6.splitTime(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testParseForHour1()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "1:30pm";
+        Integer expected = 13;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testParseForHour2()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "2:30pm";
+        Integer expected = 14;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForHour3()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "3:30pm";
+        Integer expected = 15;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForHour4()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "7:00pm";
+        Integer expected = 19;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForHour5()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "1:30am";
+        Integer expected = 1;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testParseForHour6()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "2:30am";
+        Integer expected = 2;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForHour7()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "3:30am";
+        Integer expected = 3;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForHour8()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "7:00am";
+        Integer expected = 7;
+
+        //When
+        Integer actual = problem6.parseForHour(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForMinute1()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "4:45am";
+        Integer expected = 45;
+
+        //When
+        Integer actual = problem6.parseForMinute(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseForMinute2()
+    {
+        //Given
+        Problem6 problem6 = new Problem6();
+        String timeInput = "4:03am";
+        Integer expected = 03;
+
+        //When
+        Integer actual = problem6.parseForMinute(timeInput);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+
+
 }
